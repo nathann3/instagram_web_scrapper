6e14975927c1059f136bb6e2b722656b26dc2984
roostingcat <59952641+roostingcat@users.noreply.github.com>
2020-04-30 14:39:16 -0700

    initial commit

---
+++ scrapper.py
@@ -0,0 +1,123 @@
+import time
+import pandas as pd
+import urllib.request
+import io
+import base64
+import re
+
+from IPython.display import HTML
+from io import BytesIO
+from selenium.webdriver import Firefox
+from PIL import Image
+
+
+class Scraper:
+
+    def __init__(self, tag, n=10):
+        self.tag = tag
+        self.n = n
+        self.browser = Firefox()
+        self.post_urls = self.get_post_urls(tag, n)
+        self.image_urls = self.get_image_urls()
+
+    def get_post_urls(self, tag, n):
+        tag = self.tag
+        n = self.n
+        self.browser.get('https://www.instagram.com/explore/tags/%s' % (tag))
+        lis = self.browser.find_elements_by_tag_name('a')
+        post_links = []
+        for web_element in lis:
+            if 'https://www.instagram.com/p' in web_element.get_attribute('href'):
+                post_links.append(web_element.get_attribute('href'))
+        top_posts = post_links[:n-1]
+        return top_posts
+
+    def get_image_urls(self):
+        posts = self.post_urls
+        url_list = []
+        for post_link in posts:
+            self.browser.get(post_link)
+            lis = self.browser.find_elements_by_tag_name('img')
+            url_list.append(lis[1].get_attribute('src'))
+        return url_list
+
+    def get_image(self):
+        url_list = self.image_urls
+        pictures_arrays = []
+        for url in url_list:
+            img = io.BytesIO(urllib.request.urlopen(url).read())
+            image = Image.open(img)
+            image.thumbnail((150, 150), Image.LANCZOS)
+            pictures_arrays.append(image)
+        return pictures_arrays
+
+    def get_username(self):
+        username_list = []
+        for url in self.post_urls:
+            self.browser.get(url)
+            path = '/html/body/div[1]/section/main/div/div/article/header/div[2]/div[1]/div[1]/a'
+            lis = self.browser.find_element_by_xpath(path).text
+            username_list.append(lis)
+        return username_list
+
+    def get_post_likes(self):
+        likes_list = []
+        for url in self.post_urls:
+            self.browser.get(url)
+            try:
+                path = '/html/body/div[1]/section/main/div/div[1]/article/div[2]/section[2]/div/div/button/span'
+                likes = self.browser.find_element_by_xpath(path).text
+            except:
+                button_path = '/html/body/div[1]/section/main/div/div[1]/article/div[2]/section[2]/div/span'
+                self.browser.find_element_by_xpath(button_path).click()
+                video_path = '/html/body/div[1]/section/main/div/div[1]/article/div[2]/section[2]/div/div/div[4]/span'
+                likes = self.browser.find_element_by_xpath(video_path).text
+            likes_list.append(likes)
+        return likes_list
+
+    def get_post_datetime(self):
+        # time is in UTC
+        time_list = []
+        for url in self.post_urls:
+            self.browser.get(url)
+            lis = self.browser.find_elements_by_tag_name('time')
+            try:
+                time = lis[0].get_attribute('datetime')
+                time_list.append(time)
+            except:
+                pass
+
+        return time_list
+
+    def get_post_captions(self):
+        post_list = self.post_urls
+        caption_list = []
+        for post_link in post_list:
+            self.browser.get(post_link)
+            path = '/html/body/div[1]/section/main/div/div[1]/article/div[2]/div[1]/ul/div/li/div/div/div[2]/span'
+            lis = self.browser.find_element_by_xpath(path).text
+            caption_list.append(lis)
+        return caption_list
+
+    def create_df(self):
+        data = {
+            "post_url": self.post_urls,
+            "image": self.get_image(),
+            "image_url": self.image_urls,
+            "username": self.get_username(),
+            "picture_caption": self.get_post_captions(),
+            "picture_likes": self.get_post_likes(),
+            "datetime_posted": self.get_post_datetime()
+        }
+        df = pd.DataFrame(data)
+        df["datetime_posted"] = pd.to_datetime(df["datetime_posted"])
+        df['hashtags'] = df['picture_caption'].str.findall(r"#\w+")
+        return df
+
+
+def main():
+    corg = Scraper('corgis')
+    print(corg.create_df())
+
+if __name__ == "__main__":
+    main()
