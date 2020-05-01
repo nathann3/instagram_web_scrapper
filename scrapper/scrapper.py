c427ae3c59a68742f8d8ee41a584a2aec8098854
roostingcat <59952641+roostingcat@users.noreply.github.com>
2020-04-30 20:28:21 -0700

    files(scrapper): restructure python files

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -1,9 +1,4 @@
-import pandas as pd
-import urllib.request
-import io
-
 from selenium.webdriver import Firefox
-from PIL import Image
 
 class Scrapper:
 
@@ -82,61 +77,3 @@ class Scrapper:
         except:
             return ''
         return caption
-
-
-class Posts:
-
-    scrape = Scrapper()
-
-    def __init__(self, tag, n=9):
-        self.tag = tag
-        self.n = n
-        self.browser = Firefox()
-        self.post_urls = self.get_post_urls(tag, n)
-        self.scrape = self.post_urls
-
-    def get_post_urls(self, tag, n):
-        tag = self.tag
-        n = self.n
-        self.browser.get('https://www.instagram.com/explore/tags/%s' % (tag))
-        lis = self.browser.find_elements_by_tag_name('a')
-        post_links = []
-        for web_element in lis:
-            if 'https://www.instagram.com/p' in web_element.get_attribute('href'):
-                post_links.append(web_element.get_attribute('href'))
-        top_posts = post_links[:n]
-        return top_posts
-
-    def get_image(self, url_list):
-        pictures_arrays = []
-        for url in url_list:
-            img = io.BytesIO(urllib.request.urlopen(url).read())
-            image = Image.open(img)
-            image.thumbnail((150, 150), Image.LANCZOS)
-            pictures_arrays.append(image)
-        return pictures_arrays
-
-    def create_df(self):
-        data = {
-            "post_url": self.post_urls,
-            "image": self.get_image(self.scrape['image_urls']),
-            "image_url": self.scrape['image_urls'],
-            "username": self.scrape['username'],
-            "picture_caption": self.scrape['caption'],
-            "picture_likes": self.scrape['likes'],
-            "datetime_posted": self.scrape['time'],
-        }
-        df = pd.DataFrame(data)
-        df["datetime_posted"] = pd.to_datetime(df["datetime_posted"])
-        df['hashtags'] = df['picture_caption'].str.findall(r"#\w+")
-        return df
-
-
-def main():
-    corg = Posts('corgis', 3)
-    df = corg.create_df()
-    print(df)
-    # print(corg.likes)  # the goal
-
-if __name__ == "__main__":
-    main()
