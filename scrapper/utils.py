9ff9a272f6c9b4e133b0fcd6a35e47a10d76e401
roostingcat <59952641+roostingcat@users.noreply.github.com>
2020-05-07 15:33:10 -0700

    refactor(utils): add utils to posts

--- scrapper/utils.py
+++
@@ -1,7 +0,0 @@
-def user_or_tag(term):
-    if term.startswith("#"):
-        term = term.lstrip('#')
-        url = 'https://www.instagram.com/explore/tags/%s' % (term)
-    else:
-        url = "https://www.instagram.com/%s" % (term)
-    return url
