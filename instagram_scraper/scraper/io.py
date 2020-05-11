e761ae23c9cd30c28edc2b6f4db3501489cff4f6
roostingcat <59952641+roostingcat@users.noreply.github.com>
2020-05-10 20:20:55 -0700

    docs(io): add comments and docstrings

--- instagram_scraper/scraper/io.py
+++ instagram_scraper/scraper/io.py
@@ -9,7 +9,6 @@ from PIL import Image
 from tempfile import TemporaryDirectory
 
 
-
 class SuffixWriter(AtomicWriter):
     """Similar to AtomicWriter but the temp file will have
     the exact same extension(s) as the target file"""
@@ -20,7 +19,6 @@ class SuffixWriter(AtomicWriter):
         # extracts extension (even multiple extensions) from target
         list_extension = pathlib.Path(self._path).suffixes
         self.extension = "".join(list_extension)
-
 
     def get_fileobject(self, **kwargs):
         """Return the temporary file with correct extension(s) to use"""
@@ -43,24 +41,50 @@ def atomic_write(file, mode="w", as_file=True, new_default="asdf", **kwargs):
         else:
             yield f.name
 
+
 def atomic_directory(array, fileglob, directory, ntimes):
+    """
+    atomically writes images to directory only if all images are written
+    :param array: array of images objects
+    :param fileglob: naming pattern to save each image to
+    :param directory: name of directory to create and write to
+    :param ntimes: number of images to save
+    :return: None
+    """
+
+    # checks if directory already exists
     if not os.path.exists(directory):
         with TemporaryDirectory() as tmp:
-            os.mkdir(tmp+'/t')
+
+            # create temp dir to rename later
+            os.mkdir(tmp + "/t")
             for n in range(ntimes):
-                file_name = fileglob.replace('*', str(n))
+
+                # create file name to save image to
+                file_name = fileglob.replace("*", str(n))
                 temp_name = tmp + "/t/" + file_name
                 with atomic_write(temp_name, overwrite=True) as f:
+
+                    # opens and saves image to temp file
                     request = urllib.request.urlopen(array[n]).read()
                     img = io.BytesIO(request)
                     image = Image.open(img)
+
+                    # scale down image
                     image.thumbnail((100, 100), Image.LANCZOS)
                     image.save(f)
 
+                # check if number of files are correct and last file exists, then renames directory
                 if (
-                        os.path.exists(tmp + "/t/" + fileglob.replace("*", str(ntimes - 1)))
-                        and len([name for name in os.listdir(tmp + "/t/") if os.path.isfile(tmp+"/t/"+name)])
-                        == ntimes
+                    os.path.exists(tmp + "/t/" + fileglob.replace("*", str(ntimes - 1)))
+                    and len(
+                        [
+                            name
+                            for name in os.listdir(tmp + "/t/")
+                            if os.path.isfile(tmp + "/t/" + name)
+                        ]
+                    )
+                    == ntimes
                 ):
                     os.rename(tmp + "/t", directory)
     else:
